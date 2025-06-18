2847def9f57aa894433fc3a1ed838eca6679f191
Cristian Guzman <cristianguz75@gmail.com>
2025-06-17 23:00:41 -0500

    Terminados los ejercicios de loops, ahora resolviendo los functions-exercises, quedan pendiente algunos

--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -9,8 +9,8 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 
 let num = 0
 while (num <= 20) {
-    console.log(num)
-    num++
+  console.log(num)
+  num++
 }
 
 // for (let i = 1; i < 21; i++) {
@@ -22,36 +22,93 @@ while (num <= 20) {
 num = 0
 let total = 0
 do {
-    total += num
-    num++
+  total += num
+  num++
 } while (num <= 100)
 console.log(total)
 
-
 total = 0
 for (let i = 0; i <= 100; i++) {
-    total += i
+  total += i
 }
 console.log(total)
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
 
 for (let i = 1; i <= 50; i++) {
-    if ((i % 2) == 0) {
-        console.log(i)
-    }
+  if (i % 2 == 0) {
+    console.log(i)
+  }
 }
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
 
+let nombres = ['cris', 'antonio', 'beto', 'paco']
+for (let valor of nombres) {
+  console.log(valor)
+}
+
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+
+let cadena = 'murcielago'
+let contador = 0
+for (let letra of cadena) {
+  if ('aeiou'.includes(letra.toLowerCase())) {
+    contador++
+  }
+}
+
+console.log(contador)
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
 
+let numeros = [2, 2, 3]
+let res = numeros.reduce((acc, el) => {
+  return acc * el
+})
+
+console.log(res)
+
+let mult = 1
+for (let num of numeros) {
+  mult *= num
+}
+
+console.log(mult)
+
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+let digito = 5
+for (let i = 0; i <= 10; i++) {
+  console.log(digito * i)
+}
 
-// 8. Usa un bucle para invertir una cadena de texto
+// 8. Usa un bucle para invertir una cadena de texto\
 
+cadena = 'Cadena'
+let cadenaFormateada = ''
+for (let letra of cadena) {
+  cadenaFormateada = letra + cadenaFormateada
+}
+console.log(cadenaFormateada)
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
 
+let ini1 = 0
+let ini2 = 1
+for (let i = 0; i <= 10; i++) {
+  console.log(ini1)
+  let temp = ini1 + ini2
+  ini1 = ini2
+  ini2 = temp
+}
+
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+
+numeros = [10, 20, 15, 5, 12]
+let arr = []
+for (let num of numeros) {
+  if (num > 10) {
+    arr.push(num)
+  }
+}
+
+console.log(arr)
--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -7,13 +7,76 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 
 // 1. Crea una función que reciba dos números y devuelva su suma
 
+function suma(a, b) {
+  return a + b
+}
+
+console.log(suma(2, 4))
+
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+
+function numeroMayor(arr) {
+  let num = 0
+  let mayor = 0
+  arr.forEach((element) => {
+    mayor = element > num ? element : num
+    num = mayor
+  })
+  return mayor
+}
+console.log(numeroMayor([1, 2, 3, 4, 11, 7, 9, 2]))
+
+function numeroMayor2(arr) {
+  let mayor = arr[0]
+  arr.forEach((element) => {
+    if (element > mayor) {
+      mayor = element
+    }
+  })
+  return mayor
+}
+
+console.log(numeroMayor2([1, 2, 3, 4, 11, 7, 9, 2]))
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
+function numeroVocales(cadena) {
+  let contador = 0
+  for (let letra of cadena.toLowerCase()) {
+    if ('aeiou'.includes(letra)) contador++
+  }
+  return contador
+}
+
+console.log(numeroVocales('Cadena de texto'))
+
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
 
+function textoEnMayusucula(arrText) {
+  let arrNew = []
+  for (let cadena of arrText) {
+    arrNew.push(cadena.toUpperCase())
+  }
+  return arrNew
+}
+
+console.log(textoEnMayusucula(['cris', 'antonio', 'beto', 'paco']))
+
+function textoEnMayuscula2(arr) {
+  let arrNew = []
+  arr.forEach((element) => {
+    arrNew.push(element.toUpperCase())
+  })
+  return arrNew
+}
+
+console.log(textoEnMayuscula2(['cris', 'antonio', 'beto', 'paco']))
+
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+
+function numeroPrimo(num){
+    if(num)
+}
 
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
 
