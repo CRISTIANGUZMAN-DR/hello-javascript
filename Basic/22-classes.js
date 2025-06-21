ac0e06ecadcfcc89f1019e4860389339fff83e26
Cristian Guzman <cristianguz75@gmail.com>
2025-06-20 22:39:17 -0500

    Terminacion de ejercicios de clases, pendientes ejercicios de control de errores

--- Basic/22-classes.js
+++ Basic/22-classes.js
@@ -6,19 +6,17 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=16864
 // Clases
 
 class Person {
-
-    constructor(name, age, alias) {
-        this.name = name
-        this.age = age
-        this.alias = alias
-    }
-
+  constructor(name, age, alias) {
+    this.name = name
+    this.age = age
+    this.alias = alias
+  }
 }
 
 // Sintaxis
 
-let person = new Person("Brais", 37, "MoureDev")
-let person2 = new Person("Brais", 37, "MoureDev")
+let person = new Person('Brais', 37, 'MoureDev')
+let person2 = new Person('Brais', 37, 'MoureDev')
 
 console.log(person)
 console.log(person2)
@@ -28,70 +26,64 @@ console.log(typeof person)
 // Valores por defecto
 
 class DefaultPerson {
-
-    constructor(name = "Sin nombre", age = 0, alias = "Sin alias") {
-        this.name = name
-        this.age = age
-        this.alias = alias
-    }
-
+  constructor(name = 'Sin nombre', age = 0, alias = 'Sin alias') {
+    this.name = name
+    this.age = age
+    this.alias = alias
+  }
 }
 
-let person3 = new DefaultPerson("Brais", 37)
+let person3 = new DefaultPerson('Brais', 37)
 
 console.log(person3)
 
 // Acceso a propiedades
 
 console.log(person3.alias)
-console.log(person3["alias"])
+console.log(person3['alias'])
 
-person3.alias = "MoureDev"
+person3.alias = 'MoureDev'
 
 console.log(person3.alias)
 
 // Funciones en clases
 
 class PersonWithMethod {
+  constructor(name, age, alias) {
+    this.name = name
+    this.age = age
+    this.alias = alias
+  }
 
-    constructor(name, age, alias) {
-        this.name = name
-        this.age = age
-        this.alias = alias
-    }
-
-    walk() {
-        console.log("La persona camina.")
-    }
-
+  walk() {
+    console.log('La persona camina.')
+  }
 }
 
-let person4 = new PersonWithMethod("Brais", 37, "MoureDev")
+let person4 = new PersonWithMethod('Brais', 37, 'MoureDev')
 person4.walk()
 
 // Propiedades privadas
 
 class PrivatePerson {
+  #bank
 
-    #bank
+  constructor(name, age, alias, bank) {
+    this.name = name
+    this.age = age
+    this.alias = alias
+    this.#bank = bank
+  }
 
-    constructor(name, age, alias, bank) {
-        this.name = name
-        this.age = age
-        this.alias = alias
-        this.#bank = bank
-    }
-
-    pay() {
-        this.#bank
-    }
-
+  pay() {
+    this.#bank
+  }
 }
 
-let person5 = new PrivatePerson("Brais", 37, "MoureDev", "IBAN123456789")
+let person5 = new PrivatePerson('Brais', 37, 'MoureDev', 'IBAN123456789')
 
 // No podemos acceder
-// console.log(person5.bank) 
+// console.log(person5.bank)
 // person5.bank = "new IBAN123456789" // bank no es #bank
 
 console.log(person5)
@@ -99,35 +91,33 @@ console.log(person5)
 // Getters y Setters
 
 class GetSetPerson {
+  #name
+  #age
+  #alias
+  #bank
 
-    #name
-    #age
-    #alias
-    #bank
+  constructor(name, age, alias, bank) {
+    this.#name = name
+    this.#age = age
+    this.#alias = alias
+    this.#bank = bank
+  }
 
-    constructor(name, age, alias, bank) {
-        this.#name = name
-        this.#age = age
-        this.#alias = alias
-        this.#bank = bank
-    }
+  get name() {
+    return this.#name
+  }
 
-    get name() {
-        return this.#name
-    }
-
-    set bank(bank) {
-        this.#bank = bank
-    }
-
+  set bank(bank) {
+    this.#bank = bank
+  }
 }
 
-person6 = new GetSetPerson("Brais", 37, "MoureDev", "IBAN123456789")
+person6 = new GetSetPerson('Brais', 37, 'MoureDev', 'IBAN123456789')
 
 console.log(person6)
 console.log(person6.name)
 
-person6.bank = "new IBAN123456789"
+person6.bank = 'new IBAN123456789'
 
 /*
 Clase 38 - Herencia de clases
@@ -137,57 +127,50 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=17999
 // Herencia
 
 class Animal {
+  constructor(name) {
+    this.name = name
+  }
 
-    constructor(name) {
-        this.name = name
-    }
-
-    sound() {
-        console.log("El animal emite un sonido genérico")
-    }
-
+  sound() {
+    console.log('El animal emite un sonido genérico')
+  }
 }
 
 class Dog extends Animal {
+  sound() {
+    console.log('Guau!')
+  }
 
-    sound() {
-        console.log("Guau!")
-    }
-
-    run() {
-        console.log("El perro corre")
-    }
-
+  run() {
+    console.log('El perro corre')
+  }
 }
 
 class Fish extends Animal {
+  constructor(name, size) {
+    super(name)
+    this.size = size
+  }
 
-    constructor(name, size) {
-        super(name)
-        this.size = size
-    }
-
-    swim() {
-        console.log("El pez nada")
-    }
-
+  swim() {
+    console.log('El pez nada')
+  }
 }
 
-let myDog = new Dog("MoureDog")
+let myDog = new Dog('MoureDog')
 myDog.run()
 myDog.sound()
 
-let myFish = new Fish("MoureFish", 10)
+let myFish = new Fish('MoureFish', 10)
 myFish.swim()
 myFish.sound()
 
 // Métodos estáticos
 
 class MathOperations {
-
-    static sum(a, b) {
-        return a + b
-    }
+  static sum(a, b) {
+    return a + b
+  }
 }
 
 console.log(MathOperations.sum(5, 10))
--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -4,21 +4,67 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=18630
 */
 
 // 1. Crea una clase que reciba dos propiedades
-
 // 2. Añade un método a la clase que utilice las propiedades
-
 // 3. Muestra los valores de las propiedades e invoca a la función
-
 // 4. Añade un método estático a la primera clase
-
 // 5. Haz uso del método estático
+
+class Personaje {
+  constructor(name, power) {
+    this.name = name
+    this.power = power
+  }
+
+  presentarse() {
+    console.log('Hola ' + this.name + ' haz un ' + this.power)
+  }
+
+  static fusionarse() {
+    console.log('Preparandome para fusionarme')
+  }
+
+  puntoDebil() {
+    console.log('Mi punto debil sera...')
+  }
+}
+
+let goku = new Personaje('Goku', 'Kamehameha')
+console.log(goku.name)
+console.log(goku.power)
+goku.presentarse()
+Personaje.fusionarse()
 
 // 6. Crea una clase que haga uso de herencia
 
 // 7. Crea una clase que haga uso de getters y setters
-
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
-
 // 9. Utiliza los get y set y muestra sus valores
-
 // 10. Sobrescribe un método de una clase que utilice herencia
+
+class Terricola extends Personaje {
+  #superPoder
+  constructor(name, power, superPower) {
+    super(name, power)
+    this.#superPoder = superPower
+  }
+
+  get superPower() {
+    return this.#superPoder
+  }
+
+  set superPower(superPower) {
+    this.#superPoder = superPower
+  }
+
+  puntoDebil() {
+    console.log('Punto debil las chicas')
+  }
+}
+
+let krillin = new Terricola('krillin', 'espadn', 'volar')
+console.log(krillin.superPower)
+krillin.superPower = 'macancosapo'
+console.log(krillin.superPower)
+krillin.presentarse()
+krillin.puntoDebil()
+// krillin.fusionarse() // no funciona
